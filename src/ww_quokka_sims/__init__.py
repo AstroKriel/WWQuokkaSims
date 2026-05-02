f8197deb0de3655f0b6dedbfd02c2fd3d602e8bb
Neco Kriel <necokriel@gmail.com>
2026-05-03 00:45:33 +0200

    add(ww_quokka_sims/): __init__.py files for top-level package, sim_io, and sim_manager.

---
+++ src/ww_quokka_sims/__init__.py
@@ -0,0 +1,8 @@
+from .sim_io import FieldKey as FieldKey
+from .sim_io import HelmholtzKineticEnergy as HelmholtzKineticEnergy
+from .sim_io import QuokkaDataset as QuokkaDataset
+from .sim_io import YT_SFIELD_KEYS as YT_SFIELD_KEYS
+from .sim_io import YT_VFIELD_KEYS as YT_VFIELD_KEYS
+from .sim_io import get_latest_dataset_dirs as get_latest_dataset_dirs
+from .sim_io import looks_like_boxlib_dir as looks_like_boxlib_dir
+from .sim_io import resolve_dataset_dirs as resolve_dataset_dirs
---
+++ src/ww_quokka_sims/sim_io/__init__.py
@@ -0,0 +1,8 @@
+from .find_datasets import get_latest_dataset_dirs as get_latest_dataset_dirs
+from .find_datasets import looks_like_boxlib_dir as looks_like_boxlib_dir
+from .find_datasets import resolve_dataset_dirs as resolve_dataset_dirs
+from .load_dataset import FieldKey as FieldKey
+from .load_dataset import HelmholtzKineticEnergy as HelmholtzKineticEnergy
+from .load_dataset import QuokkaDataset as QuokkaDataset
+from .load_dataset import YT_SFIELD_KEYS as YT_SFIELD_KEYS
+from .load_dataset import YT_VFIELD_KEYS as YT_VFIELD_KEYS
