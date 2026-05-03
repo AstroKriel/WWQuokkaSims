bffb12efd0ddcbaeb40df9491a0c6e09942df246
Neco Kriel <necokriel@gmail.com>
2026-05-03 19:16:59 +0200

    del(__init__.py): remove re-exports; users import submodules directly.

--- src/ww_quokka_sims/__init__.py
+++
@@ -1,8 +0,0 @@
-from .sim_io import FieldKey as FieldKey
-from .sim_io import HelmholtzKineticEnergy as HelmholtzKineticEnergy
-from .sim_io import QuokkaSnapshot as QuokkaSnapshot
-from .sim_io import YT_SFIELD_KEYS as YT_SFIELD_KEYS
-from .sim_io import YT_VFIELD_KEYS as YT_VFIELD_KEYS
-from .sim_io import get_latest_snapshot_dirs as get_latest_snapshot_dirs
-from .sim_io import looks_like_boxlib_dir as looks_like_boxlib_dir
-from .sim_io import resolve_snapshot_dirs as resolve_snapshot_dirs
