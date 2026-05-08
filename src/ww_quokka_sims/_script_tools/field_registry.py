5892c7ac0a999d6ecae770bc3dc7158b22fcccfb
Neco Kriel <necokriel@gmail.com>
2026-05-08 19:03:51 +0200

    del(field_registry.py): remove _field_entry; instantiate FieldEntry directly in QUOKKA_FIELD_LOOKUP.

--- src/ww_quokka_sims/_script_tools/field_registry.py
+++ src/ww_quokka_sims/_script_tools/field_registry.py
@@ -5,6 +5,7 @@
 ##
 
 from collections.abc import Callable
+from dataclasses import dataclass
 
 from ww_quokka_sims.sim_io import load_snapshot
 
@@ -20,89 +21,85 @@ DIVERGING_CMAP = "cmr.iceburn"
 ##
 
 
-def _field_entry(
-    loader: Callable,
-    cmap: str,
-) -> dict:
-    return {
-        "loader": loader,
-        "cmap": cmap,
-    }
+@dataclass(frozen=True)
+class FieldEntry:
+    loader: Callable
+    cmap: str
 
 
 QUOKKA_FIELD_LOOKUP = {
     "rho":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.load_3d_density_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "vel":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_velocity_vfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "vel_magn":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_velocity_magnitude_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "mag":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.load_3d_magnetic_vfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "E_tot":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.load_3d_total_energy_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "E_int":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_internal_energy_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "E_kin":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_kinetic_energy_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "E_kin_div":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_div_kinetic_energy_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "E_kin_sol":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_sol_kinetic_energy_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "E_kin_bulk":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_bulk_kinetic_energy_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "E_mag":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_magnetic_energy_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "E_ratio":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_energy_ratio_sfield,
         cmap=DIVERGING_CMAP,
     ),
     "pressure":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_pressure_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
     "divb":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_div_b_sfield,
         cmap=DIVERGING_CMAP,
     ),
     "cur":
-    _field_entry(
+    FieldEntry(
         loader=load_snapshot.QuokkaSnapshot.compute_current_density_sfield,
         cmap=SEQUENTIAL_CMAP,
     ),
