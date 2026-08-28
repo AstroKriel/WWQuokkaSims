9b5b85ca37969ca6ec42c6134a45c917fa19965c
Neco Kriel <necokriel@gmail.com>
2026-08-28 18:45:33 +0200

    add-fn(run): save domain metadata before extracting fields.

--- src/ww_quokka_sims/_diagnostic_scripts/extract_dataset.py
+++ src/ww_quokka_sims/_diagnostic_scripts/extract_dataset.py
@@ -31,6 +31,7 @@ from ww_quokka_sims._script_tools import (
     field_registry,
 )
 from ww_quokka_sims.sim_io import (
+    domain_cache,
     find_snapshots,
     load_snapshot,
 )
@@ -355,6 +356,17 @@ class ScriptInterface:
         index_width = find_snapshots.get_max_index_width(
             snapshot_dirs=snapshot_dirs,
             snapshot_tag=self.snapshot_tag,
+        )
+        with load_snapshot.QuokkaSnapshot(
+                snapshot_dir=snapshot_dirs[0],
+                verbose=False,
+        ) as snapshot:
+            uniform_domain_3d = snapshot.load_3d_uniform_domain(amr_level=self.amr_level)
+        domain_cache.save_domain(
+            uniform_domain_3d=uniform_domain_3d,
+            amr_level=self.amr_level,
+            data_dir=data_dir,
+            overwrite=self.overwrite,
         )
         if (self.num_workers != 1) and (len(snapshot_dirs) > 5):
             extract_fields_in_parallel(
