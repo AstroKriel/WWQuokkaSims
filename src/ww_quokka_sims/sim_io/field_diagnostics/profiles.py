a36393562b25e6b74ebaba741a5fa42ba979f33a
Neco Kriel <necokriel@gmail.com>
2026-09-09 12:20:30 +0200

    refactor-fn(run): iterate comp_profiles_lookup.values(), don't re-index by key.

--- src/ww_quokka_sims/sim_io/field_diagnostics/profiles.py
+++ src/ww_quokka_sims/sim_io/field_diagnostics/profiles.py
@@ -888,8 +888,8 @@ class GenerateCompProfiles:
                     if comp_profile.comp_name not in comp_profiles_lookup:
                         comp_profiles_lookup[comp_profile.comp_name] = []
                     comp_profiles_lookup[comp_profile.comp_name].append(comp_profile)
-            for comp_name in comp_profiles_lookup:
-                comp_profiles_lookup[comp_name].sort(key=lambda _comp_profile: _comp_profile.sim_time)
+            for comp_profiles in comp_profiles_lookup.values():
+                comp_profiles.sort(key=lambda _comp_profile: _comp_profile.sim_time)
             self._save_summary_figure(
                 comp_profiles_lookup=comp_profiles_lookup,
                 figures_dir=self.figures_dir,
